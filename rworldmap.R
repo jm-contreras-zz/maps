6fa51b4b5ca8523612ff9a084269730b34432636
Juan Manuel Contreras <juan.manuel.contreras.87@gmail.com>
2014-03-30 17:42:31 -0400

    Update rworldmap.R

--- rworldmap.R
+++ rworldmap.R
@@ -1,3 +1,9 @@
+# rworldmap.R draws a map of the world with countries color-coded by
+# population rank.
+#
+# Written by Juan Manuel Contreras (juan.manuel.contreras.87@gmail.com)
+# on March 30, 2014.
+
 # Load libraries
 library(rworldmap)
 library(RColorBrewer)
